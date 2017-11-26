42312a981d44c55331f2baa57867c625aefa58a7
roy_pearl <royp@sentinelone.com>
2017-11-26 11:27:20 +0200

    AntsPublisher port update

--- antspublisher/src/main/java/com/tikalk/antspublisher/Constants.java
+++ antspublisher/src/main/java/com/tikalk/antspublisher/Constants.java
@@ -1,7 +1,6 @@
 package com.tikalk.antspublisher;
 
 public class Constants {
-    public static final int DEFAULT_PORT = 8090;
-    public static final String CALL_URL_ADDRESS = "call.url";
-    public static final String PROCESS_REQUEST_ADDRESS = "process.request";
+    public static final int DEFAULT_PORT = 8082;
+
 }
