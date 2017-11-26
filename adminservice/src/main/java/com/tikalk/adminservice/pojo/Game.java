4324fa949d89fbf88e0d758f814859ee631e21d4
Ran Silberman <ran.silberman@feedvisor.com>
2017-11-26 16:20:43 +0200

    First commit by Ran

---
+++ adminservice/src/main/java/com/tikalk/adminservice/pojo/Game.java
@@ -0,0 +1,88 @@
+package com.tikalk.adminservice.pojo;
+
+import java.util.Date;
+
+public class Game {
+
+        private Integer id;
+        private Date createdAt;
+        private Date updatedAt;
+        private String state;
+        private Date startTime;
+        private Date stopTime;
+        private Date resumeTime;
+        private Date finishTime;
+        private long gameTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(Date stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public Date getResumeTime() {
+        return resumeTime;
+    }
+
+    public void setResumeTime(Date resumeTime) {
+        this.resumeTime = resumeTime;
+    }
+
+    public Date getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(Date finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public long getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime(long gameTime) {
+        this.gameTime = gameTime;
+    }
+}
